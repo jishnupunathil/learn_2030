2ffc353487b9a5103aead67f0d4c62e4eb52e38e
jishnupunathil <jishnupunathil000@gmail.com>
2026-01-26 12:32:22 +0100

    initial

---
+++ age.js
@@ -0,0 +1,22 @@
+function ageCalc(birthYear){
+
+    const currentYear=2026
+    return age = currentYear-birthYear
+
+}
+
+console.log(ageCalc(1999))
+
+
+function checkEvenOrOdd(number){
+    if(number % 2 === 0) return "Even"
+    return "odd"
+}
+
+console.log(checkEvenOrOdd(2743));
+
+const celToFah=(celsius)=>{
+     return (celsius * 9) / 5 + 32;
+}
+
+console.log(celToFah(-8))
