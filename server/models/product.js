ac76664598456dbc968e4017e5cee23428fedb53
Jishnu Punathil <jishnupunathil000@gmail.com>
2026-02-17 20:05:46 +0100

    Add prodcut model

---
+++ server/models/product.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    countInStock: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
