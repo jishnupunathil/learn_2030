d4718c31a443dcc1b43789b3aed5061507ff482d
Jishnu Punathil <jishnupunathil000@gmail.com>
2026-03-11 23:37:22 +0100

    api for serach

--- MERn CRUD/client/src/api/userApi.js
+++ MERn CRUD/client/src/api/userApi.js
@@ -1,4 +1,4 @@
-Const API_URL = "http://localhost:5000/api/users";
+const API_URL = "http://localhost:5000/api/users";
 
 //get users with pagination
 
@@ -9,3 +9,14 @@ export const getUsers = async (page = 1, limit = 10) => {
     }
     return response.json();
 };
+
+//search users by name or email
+
+export const searchUsers=async(term='',page=1,limit=5)=>{
+    const response=await fetch(
+        `${API_URL}/search/${encodeURIComponent(term)}?page=${page}&limit=${limit}`
+    )
+    if(!response.ok) throw new Error ("Failed to search user")
+
+    return response.json()
+}
